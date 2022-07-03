64a4f0acdb03c80694e6ca90c2a4e2d8c8b02982
franckwilfried <wilfiedfranck@gmail.com>
2022-07-03 20:54:41 +0200

    possibilité de retour

--- pages/recrutement/job.js
+++ pages/recrutement/job.js
@@ -1,22 +1,30 @@
 
 /* Action du click droit pour aller du job 1 au 2 */
+let jobUn = document.querySelector(".intitule").innerText
 
-let boutonDroitUn = document.querySelector(".fa-angle-right");
-const afficheJobDeux = '<section class="job_two"> <i class="fa-solid fa-angle-left left_two"></i><div class="intitule">Cuisinier(ère)</div><div class="profil">Profil<ul> <li>Age entre 18 et 25 ans</li> <li>CAP cuisine</li> <li>Organiser et ponctuel</li> </ul> </div> <div class="mission"> Missions <ul>  <li>Netoyage</li> <li>Service caisse</li> <li>Entretient du matériel</li> </ul> </div> <i class="fa-solid fa-angle-right right_two"></i> </section> ';
+if( jobUn == "Equipier(ère) polyvalent(e)" )
+{
 
-boutonDroitUn.addEventListener('click', 
-function(){
-    document.querySelector(".principal").innerHTML = afficheJobDeux ;
-})
+    let boutonDroitUn = document.querySelector(".right_one");
+    const afficheJobDeux = '<section class="job_two"> <i class="fa-solid fa-angle-left left_two"></i><div class="intitule">Cuisinier(ère)</div><div class="profil">Profil<ul> <li>Age entre 18 et 25 ans</li> <li>CAP cuisine</li> <li>Organiser et ponctuel</li> </ul> </div> <div class="mission"> Missions <ul>  <li>Netoyage</li> <li>Service caisse</li> <li>Entretient du matériel</li> </ul> </div> <i class="fa-solid fa-angle-right right_two"></i> </section> ';
 
-/* Action du click gauche pour aller du job 2 au 1 */
+    boutonDroitUn.addEventListener('click',function()
+    {
+        document.querySelector(".principal").innerHTML = afficheJobDeux ;
 
-let boutonGaucheDeux = document.querySelector(".right_two");
-const afficheJobUn = '<section class="job_one"> <i class="fa-solid fa-angle-left left_one"></i><div class="intitule">Equipier(ère) polyvalent(e)</div><div class="profil">Profil<ul> <li>Age entre 18 et 25 ans</li> <li>CAP cuisine</li> <li>Organiser et ponctuel</li> </ul> </div> <div class="mission"> Missions <ul>  <li>Netoyage</li> <li>Service caisse</li> <li>Entretient du matériel</li> </ul> </div> <i class="fa-solid fa-angle-right right_one"></i> </section> ';
-                
-boutonGaucheDeux.addEventListener('click', 
-function(){
-    document.querySelector(".principal").innerHTML = afficheJobUn ;
-})
+        let job = document.querySelector(".intitule").innerText
+        alert(job)
+        if ( job == "Cuisinier(ère)" )
+        {
 
-alert("salut");
+            let boutonGaucheDeux = document.querySelector(".left_two");
+            const afficheJobUn = '<section class="job_one"> <i class="fa-solid fa-angle-left left_one"></i><div class="intitule">Equipier(ère) polyvalent(e)</div><div class="profil">Profil<ul> <li>Age entre 18 et 25 ans</li> <li>CAP cuisine</li> <li>Organiser et ponctuel</li> </ul> </div> <div class="mission"> Missions <ul>  <li>Netoyage</li> <li>Service caisse</li> <li>Entretient du matériel</li> </ul> </div> <i class="fa-solid fa-angle-right right_one"></i> </section> ';
+                                
+            boutonGaucheDeux.addEventListener('click',function()
+            {
+                document.querySelector(".principal").innerHTML = afficheJobUn ;
+                window.location.reload(true);
+            })
+        }
+    })
+}
